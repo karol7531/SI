66401261ca8ec8ea2f523b5704a9db80b80713a7
karol <242557@student.pwr.edu.pl>
2020-04-12 00:24:19 +0200

    connect 4 init commit

---
+++ lista3/Project/SI_3/SI_3/MinMax.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SI_3
+{
+    class MinMax
+    {
+        private int depth;
+
+        public MinMax(int depth)
+        {
+            this.depth = depth;
+        }
+
+        //return state?
+        public int Move(State state) { }
+
+        private int Max(State state, int depth) { }
+
+        private int Min(State state, int depth) { }
+    }
+}
---
+++ lista3/Project/SI_3/SI_3/State.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SI_3
+{
+    class State
+    {
+        private bool?[,] board;
+        private int rows, cols;
+
+        public State(int rows, int cols)
+        {
+            this.rows = rows;
+            this.cols = cols;
+            board = new bool?[rows, cols];
+        }
+
+        internal State Clone() { }
+
+        internal bool CanPlace(int col) { }
+
+        internal State NextState(bool player, int col) { }
+
+        public void PrintState() { }
+
+        public bool DidWin(bool player) { }
+
+
+
+    }
+}
